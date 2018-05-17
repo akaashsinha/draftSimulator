5efbc35e62e4bf2c845af92021754e5839465fd1
Akaash Sinha <grendelfearsbeowufl@gmail.com>
2018-05-16 20:41:07 -0400

    Playing around with executing the full list and adding the player pool. Not sure how I'll eventually account for all these variables, but I'll find a way.

--- PlayerList.py
+++ PlayerList.py
@@ -1,4 +1,29 @@
 from Player import *
+# Using Pipeline's rankings
+
 
 MizeCa = Player("Casey Mize", 0, "Auburn", 1, 100)
 StewartCa = Player("Carter Stewart", 0, "Eau Gallie HS", 2, 100)
+MadrigalNi = Player("Nick Madrigal", 3, "Oregon State", 3, 100)
+LiberatoreMa = Player("Matthew Liberatore", 9, "Mountain Ridge HS", 4)
+SingerBr = Player("Brady Singer", 0, "Florida", 5)
+McClanahanSh = Player("Shane McClanahan", 9, "South Florida", 6)
+SwaggertyTr = Player("Travis Swaggerty", 7, "South Alabama", 7)
+KelenicJa = Player("Jarred Kelenic", 7, "Waukesha West HS", 8)
+BartJo = Player("Joey Bart", 1, "Georgia Tech", 9)
+IndiaJo = Player("Jonathan India", 4, "Florida", 10)
+BohmAl = Player("Alec Bohm", 4, "Wichita State", 11)
+RolisonRy = Player("Ryan Rolison", 9, " Mississippi", 12)
+KowarJa = Player("Jackson Kowar", 0, "Florida", 13)
+RockerKu = Player("Kumar Rocker", 0, "North Oconee HS", 14)
+WinnCo = Player("Cole Winn", 0, "Orange Lutheran HS", 15)
+WeathersRy = Player("Ryan Weathers", 9, "Loretto HS", 16)
+GormanNo = Player("Nolan Gorman", 4, 'O\'Connor', 17)
+ScottCo = Player("Connor Scott", 7, "Plant HS", 18)
+HankinsEt = Player("Ethan Hankins", 0, "Forsyth Central HS", 19)
+DenaburgMa = Player("Mason Denaburg", 0, "Merritt Island HS", 20)
+VasilMi = Player("Mike Vasil", 0, "Boston College", 21)
+TurangBr = Player("Brice Turang", 5, "Santiago HS", 22)
+EiermanJe = Player("Jeremy Eierman", 5, "Missouri State", 23)
+
+playerPool = [MizeCa, StewartCa, MadrigalNi, LiberatoreMa, SingerBr, McClanahanSh, SwaggertyTr, KelenicJa, BartJo, IndiaJo]
--- draft.py
+++ draft.py
@@ -7,6 +7,8 @@ from DraftOrder import *
 
 for team in order:
     print("With the #{} of the MLB draft the {} pick".format(team.draftPosition, team.name))
-    time.sleep(3)
-    print("{} from {}".format(MizeCa.name, MizeCa.school))
+    time.sleep(1.5)
+    print("{} from {} a {}".format(McClanahanSh.name, McClanahanSh.school, McClanahanSh.position))
 
+for player in playerPool:
+    print(player.name, player.position)
